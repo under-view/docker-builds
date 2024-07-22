ac7f618b8c9226b5e54f5fc29242c5819d440280
Vincent Davis Jr <vince@underview.tech>
2024-07-21 21:36:17 -0400

    containers: remove sudo
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- containers/yocto-project/ubuntu-24.04/startup.sh
+++ containers/yocto-project/ubuntu-24.04/startup.sh
@@ -2,10 +2,6 @@
 
 groupadd -g "${HOST_GID}" "${HOST_GNAME}" > /dev/null 2>&1 || ret=$?
 useradd -g "${HOST_GID}" -m -s /bin/bash -u "${HOST_UID}" "${HOST_UNAME}" > /dev/null 2>&1 || ret=$?
-usermod -aG sudo "${HOST_UNAME}" > /dev/null 2>&1
-
-echo "${HOST_UNAME}:${DOCKER_PASSWORD}" | chpasswd > /dev/null 2>&1
-echo "${HOST_UNAME} ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 chown "${HOST_UNAME}":"${HOST_GNAME}" "/home/${HOST_UNAME}" > /dev/null 2>&1
 mv /env-spice.sh "/home/${HOST_UNAME}" > /dev/null 2>&1
