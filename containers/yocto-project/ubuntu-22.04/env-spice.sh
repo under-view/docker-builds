b1c0adebd4e685acddb178f079a1209c0832b4af
Vincent Davis Jr <vince@underview.tech>
2024-08-11 05:48:35 +0000

    containers[yp]: add evironment vars
    DL_DIR
    SSTATE_DIR
    PARALLEL_MAKE
    BB_NUMBER_THREADS
    BB_ENV_PASSTHROUGH_ADDITIONS
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- containers/yocto-project/ubuntu-22.04/env-spice.sh
+++ containers/yocto-project/ubuntu-22.04/env-spice.sh
@@ -54,3 +54,18 @@ alias l='ls -CF'
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+export DL_DIR="${HOME}/downloads"
+export SSTATE_DIR="${HOME}/sstate-cache"
+
+# User space memory allocation is allowed to over-commit memory (more than
+# available physical memory) which can lead to out of memory errors
+# The out of memory (OOM) killer kicks in and selects a process to kill to retrieve some memory.
+# Thus why we see the bellow error at random points of the build
+#	x86_64-underview-linux-g++: fatal error: Killed signal terminated program cc1plus
+# Lowering BBTHREADS and MTHREADS ensures the system doesn't run out of memory when building
+core_count=$(nproc)
+export PARALLEL_MAKE="-j $((core_count / 2))"
+export BB_NUMBER_THREADS=$((core_count / 2))
+
+export BB_ENV_PASSTHROUGH_ADDITIONS="DL_DIR SSTATE_DIR UNDERVIEW_IMAGE_TYPE"
